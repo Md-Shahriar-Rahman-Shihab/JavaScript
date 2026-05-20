d65aaa6dd15705adc7930ff5b74b962254ca2564
Md-Shahriar-Rahman-Shihab <shihabshahriar21821@gmail.com>
2026-05-20 12:43:27 +0600

    operators

---
+++ Phase -1/03_Operators/script.js
@@ -0,0 +1,38 @@
+//Arithmetic, comparison , logical, assignment, unary, ternary
+
+// + - * / **
+// = == === 
+// < > <= >= ! != !== !!
+// && || 
+// ? : 
+
+
+// Arithmetic operators + - * / ** %
+
+// +
+
+let sum= 1+2; //3
+let name= "Shih"+"ab" //"Shihab" concatenation
+
+
+//-
+let subtract = 12-2 //10
+
+//*
+let multiply = 12*5 //60
+
+// /
+let divided = 12/6 //2
+
+// % modulo
+let modulo = 12/5 //2
+
+// **
+let power = 2 ** 3 //2*2*2
+
+
+// comparison operators  == === != !== >= <= > < 
+
+// == 
+let equal = 12 == 13 //false 
+let equal2 = 12 == "12" //true [double equal only check values not check data types here 12 is number but "12" is string]
